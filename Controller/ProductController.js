28f16e6437ef9a22aa302ac1b0cb4226903ddb93
florencialp <f.lopez.pacini@gmail.com>
2023-07-08 14:21:21 -0300

    add endpoint to get all user products

--- Controller/ProductController.js
+++ Controller/ProductController.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const protectSession = require("../Middleware/AuthMiddleware");
-const { createProduct } = require("../Service/ProductService");
+const { createProduct, getProducts } = require("../Service/ProductService");
 const { uploadFile } = require("../Utils/UploadFile");
 
 router.post('/', protectSession, uploadFile.single("photo"), createProduct);
+router.get('/', protectSession, getProducts);
 
 module.exports = router;
--- Service/ProductService.js
+++ Service/ProductService.js
@@ -45,6 +45,12 @@ const createProduct = asyncHandler(async (req, res) => {
     res.status(201).json(product);
 });
 
+const getProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({ user: req.user.id }).sort("-createdAt");
+    res.status(200).json(products);
+});
+
 module.exports = {
-    createProduct
+    createProduct,
+    getProducts
 };
