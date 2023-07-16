13277caa3d6bbf540dc8ee97f0869bc26c871f37
florencialp <f.lopez.pacini@gmail.com>
2023-07-16 19:38:48 -0300

    add endpoint to delete and update product

--- Controller/ProductController.js
+++ Controller/ProductController.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const protectSession = require("../Middleware/AuthMiddleware");
-const { createProduct, getProducts, getProduct } = require("../Service/ProductService");
+const { createProduct, getProducts, getProduct, deleteProduct, updateProduct } = require("../Service/ProductService");
 const { uploadFile } = require("../Utils/UploadFile");
 
 router.post('/', protectSession, uploadFile.single("photo"), createProduct);
 router.get('/', protectSession, getProducts);
 router.get('/:productId', protectSession, getProduct);
+router.delete('/:productId', protectSession, deleteProduct);
+router.patch('/:productId', protectSession, uploadFile.single("photo"), updateProduct);
 
 module.exports = router;
--- Service/ProductService.js
+++ Service/ProductService.js
@@ -5,7 +5,6 @@ const cloudinary = require('cloudinary').v2;
 
 const createProduct = asyncHandler(async (req, res) => {
     const { name, sku, category, amount, price, description } = req.body;
-    console.log('0');
 
     if (!name || !category || !amount || !price || !description) {
         res.status(400);
@@ -70,8 +69,97 @@ const getProduct = asyncHandler(async (req, res) => {
     res.status(200).json(product);
 });
 
+const deleteProduct = asyncHandler(async (req, res) => {
+    if (!req.params.productId.match(/^[0-9a-fA-F]{24}$/)) {
+        res.status(400);
+        throw new Error("Invalid product ID");
+    }
+
+    const product = await Product.findById(req.params.productId);
+    if (!product) {
+        res.status(404);
+        throw new Error("Product not found");
+    }
+
+    if (product.user.toString() != req.user.id) {
+        res.status(401);
+        throw new Error("User not authorized");
+    }
+
+    await product.deleteOne();
+
+    res.status(200).json({ message: "Product deleted successfully" });
+});
+
+const updateProduct = asyncHandler(async (req, res) => {
+    const { name, sku, category, amount, price, description } = req.body;
+
+    if (!name || !category || !amount || !price || !description) {
+        res.status(400);
+        throw new Error("Fill all required fields");
+    }
+
+    const { productId } = req.params;
+
+    if (!productId.match(/^[0-9a-fA-F]{24}$/)) {
+        res.status(400);
+        throw new Error("Invalid product ID");
+    }
+    const product = await Product.findById(productId);
+    if (!product) {
+        res.status(404);
+        throw new Error("Product not found");
+    }
+
+    if (product.user.toString() != req.user.id) {
+        res.status(401);
+        throw new Error("User not authorized");
+    }
+
+    let fileData = {};
+    if (req.file) {
+        let uploadedFile;
+        try {
+            uploadedFile = await cloudinary.uploader.upload(req.file.path, { folder: "Pinvent app", resourse_type: "image" });
+        } catch (error) {
+            res.status(500);
+            throw new Error("Image upload failed");
+        }
+
+        fileData = {
+            fileName: req.file.originalname,
+            filePath: uploadedFile.secure_url,
+            fileType: req.file.mimetype,
+            fileSize: fileSizeFormatter(req.file.size, 2)
+        };
+    }
+
+    const updatedProduct = await Product.findByIdAndUpdate(
+        {
+            _id: productId
+        },
+        {
+            name,
+            sku,
+            category,
+            amount,
+            price,
+            description,
+            photo: Object.keys(fileData).length === 0 ? product?.photo : fileData
+        },
+        {
+            new: true,
+            runValidators: true
+        }
+    );
+
+    res.status(201).json(updatedProduct);
+});
+
 module.exports = {
     createProduct,
     getProducts,
-    getProduct
+    getProduct,
+    deleteProduct,
+    updateProduct
 };
